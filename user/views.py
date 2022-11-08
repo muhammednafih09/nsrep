03e7fa880c3e6cdfcddec8c48de8c728fb781f2e
muhammednafih09 <muhammednafih09@gmail.com>
2022-11-08 10:21:06 +0530

    default image added

--- user/views.py
+++ user/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout, get_user
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from dashboard.views import index
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-
 
 
 def loginUser(request):
